52500f487cd971eea8b8aa5817a8f3edaf0438c4
md tahseen <tahseen15july.tq@gmail.com>
2019-03-25 04:55:39 -0700

    update

--- lib/page/service/service.dart
+++ lib/page/service/service.dart
@@ -1,3 +1,3 @@
-final api_key = 'your_api_key';
+final api_key = 'your_api_key';  // you can registerd get your own api key from https://www.themoviedb.org/
 
 final imgUrl = 'http://image.tmdb.org/t/p/w780//';
